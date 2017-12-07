459513d6424bf4f0e66848b4478a2673d5fc54ea
Abraham Castro <cucharadepalo@gmail.com>
2017-12-07 12:44:18 +0100

    Basi question API

--- server/routes/questions.routes.js
+++ server/routes/questions.routes.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const Question = require('../models/question.model');
 
 router.get('/question', (req, res) => {
-  Question.random()
-    .then(question => res.status(200).json(question))
+  const cat = req.query.cat;
+  let filter = { approved: true };
+  cat ? filter['category'] = cat : filter;
+  Question.findRandom(filter, {}, {limit: 1})
+    .then(question => res.status(200).json(question[0]))
     .catch(e => res.status(500).json({error:e.message}));
 });
-//router.get('/question?cat');
+
+// To sincronize random Objects in the document collection
+router.get('/syncRandom', (req, res) => {
+  Question.syncRandom(function (err, result) {
+    console.log(result.updated);
+  });
+});
+
 
 module.exports = router;
