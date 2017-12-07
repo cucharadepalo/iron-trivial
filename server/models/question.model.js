c2f55128969a17e4dde15104214bf42fd5a5bbbb
Abraham Castro <cucharadepalo@gmail.com>
2017-12-07 12:43:32 +0100

    Added mongoose random plugin

--- server/models/question.model.js
+++ server/models/question.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const random = require('mongoose-random');
 const Schema = mongoose.Schema;
 const CATEGORIES = require('./categories');
 
@@ -6,7 +7,7 @@ const questionSchema = new Schema({
   creator: { type: String },
   question: { type: String, required: true },
   questionImg: { type: String, default: null },
-  questionIsImg: { type: String, default: false },
+  questionIsImg: { type: Boolean, default: false },
   questionCode: { type: String, default: null},
   questionIsCode: { type: Boolean, default: false},
   category: { type: String, enum: CATEGORIES, required: true },
@@ -14,17 +15,19 @@ const questionSchema = new Schema({
   fakeAnswers: [{ type: String, required: true }],
   approved: { type: Boolean, default: false }
 }, {
-  timestamps: { createdAt: "created_at", updatedAt: "updated_at"}
+  timestamps: true,
+  toJSON: {
+      transform: (doc, ret) => {
+          ret.id = ret._id;
+          delete ret.approved;
+          delete ret._id;
+          delete ret.__v;
+          delete ret.random;
+          return ret;
+      }
+  }
 });
-// pasar el método a las rutas (para poder hacerlo por categorías)
-questionSchema.statics.random = function(cb) {
-  this.count(function(err, count) {
-    if (err) {
-      return cb(err);
-    }
-    let rand = Math.floor(Math.random() * count);
-    this.findOne().skip(rand).exec(cb);
-  }.bind(this));
-};
+
+questionSchema.plugin(random, { path: 'random' });
 
 module.exports = mongoose.model('Question', questionSchema);
