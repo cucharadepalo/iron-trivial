a720b2252bdc4e7ef8088473077eee584bd71ed1
Abraham Castro <cucharadepalo@gmail.com>
2017-12-07 13:50:31 +0100

    api/game route working

--- server/routes/questions.routes.js
+++ server/routes/questions.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Question = require('../models/question.model');
+const CATEGORIES = require('../models/categories');
 
 router.get('/question', (req, res) => {
   const cat = req.query.cat;
@@ -8,6 +9,17 @@ router.get('/question', (req, res) => {
   cat ? filter['category'] = cat : filter;
   Question.findRandom(filter, {}, {limit: 1})
     .then(question => res.status(200).json(question[0]))
+    .catch(e => res.status(500).json({error:e.message}));
+});
+
+router.get('/game', (req, res) => {
+  let promises = CATEGORIES.map(cat => {
+    return Question.findRandom({ approved: true, category: cat }, {}, { limit: 1 });
+  });
+  Promise.all(promises)
+    .then(q => {
+      res.status(200).json( [].concat.apply([], q) );
+    })
     .catch(e => res.status(500).json({error:e.message}));
 });
 
