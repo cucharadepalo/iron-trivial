727aa04ccc3a888aabd408d2cf55f8c8dd4fca83
Abraham Castro <cucharadepalo@gmail.com>
2017-12-08 15:40:08 +0100

    Added creator, and ToJSON format

--- server/models/game.model.js
+++ server/models/game.model.js
@@ -2,17 +2,25 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const gameSchema = new Schema({
-  name: { type: String, default: 'Iron-trivia game' },
+  creator: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  name: { type: String, default: 'Iron-trivia game', required: true },
   participants: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   questions: [{ type: Schema.Types.ObjectId, ref: 'Question' }],
   ranking: [{
     position: { type: Number },
     user: { type: String },
     points: { type: Number }
-  }
-  ]
+  }]
 }, {
-  timestamps: { createdAt: "created_at", updatedAt: "updated_at"}
+  timestamps: true,
+  toJSON: {
+    transform: (doc, ret) => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+    }
+  }
 });
 
 module.exports = mongoose.model("Game", gameSchema);
