3fa72b1e5b797fef7f6376cf7cba70f728890e62
Abraham Castro <cucharadepalo@gmail.com>
2017-12-09 20:22:29 +0100

    Set redirect for auth

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -3,23 +3,33 @@ const router = express.Router();
 const passport = require('passport');
 const passportConfig = require('../config/passport.config');
 
+router.get('/user', (req, res, next) => {
+  if (req.isAuthenticated()) {
+    res.status(200).json(req.user);
+    return;
+  }
+  res.status(401).json({ message: 'Unauthorized' });
+});
+
+router.get('/logout', (req, res, next) => {
+  req.logout();
+  res.status(200).json({ message: 'Success' });
+  });
+
 router.get('/github', passport.authenticate('github'));
 router.get('/github/callback',
   passport.authenticate('github'),
   (req, res, next) => {
-    res.json(req.user);
+    res.redirect(process.env.LOGIN_CALLBACK);
+    //res.json(req.user);
   });
 
 router.get('/facebook', passport.authenticate('facebook'));
 router.get('/facebook/callback',
   passport.authenticate('facebook'),
   (req, res, next) => {
-    res.json(req.user);
-  });
-
-router.get('/logout', (req, res, next) => {
-    req.logout();
-    res.status(200).json({ message: 'Success' });
+    res.redirect(process.env.LOGIN_CALLBACK);
+    //res.json(req.user);
   });
 
 module.exports = router;
