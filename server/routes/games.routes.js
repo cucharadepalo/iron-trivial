66353f19403016fa55984584488b9757f2630931
Abraham Castro <cucharadepalo@gmail.com>
2017-12-10 15:07:24 +0100

    Added more games endpoints

--- server/routes/games.routes.js
+++ server/routes/games.routes.js
@@ -6,6 +6,7 @@ const GameUser = require('../models/game-user.model');
 const Question = require('../models/question.model');
 const CATEGORIES = require('../models/categories');
 
+// Single game object
 router.get('/game', (req, res, next) => {
   const id = req.query.id;
   if (id) {
@@ -22,7 +23,7 @@ router.get('/game', (req, res, next) => {
     res.status(400).json({message: 'You have to provide a game id by a query param'});
   }
 });
-
+// Single game creation
 router.post('/game', (req, res, next) => {
   const userId = req.body.userId;
   let promises = CATEGORIES.map(cat => {
@@ -47,7 +48,22 @@ router.post('/game', (req, res, next) => {
     })
     .catch(err => next(err));
 });
-
+// Games request
+router.get('/games', (req, res, next) => {
+  const open = req.query.open;
+  open ? finished = false : finished = true;
+  let query = {};
+  if (open !== undefined && open !== null) {
+    query = { isOpen: open, isFinished: finished };
+  }
+  Game.find().populate('questions')
+    .then(games => {
+      console.log('llego');
+      games.length > 0 ? res.json(games) : res.status.json({ message: 'There aren\'t games'});
+    })
+    .catch(err => next(err));
+});
+// Game add participants
 router.put('/game/:id', (req, res, next) => {
   const id = req.params.id;
   const participant = req.body.userId;
@@ -60,6 +76,50 @@ router.put('/game/:id', (req, res, next) => {
           if (game) {
             res.json(game);
             GameUser.create({ _gameId: game._id, _userId: participant });
+          } else {
+            res.status(404).json({ message: 'Game not found' });
+          }
+        })
+        .catch(err => next(err));
+    } else {
+      res.status(400).json({ message: 'Invalid game id' });
+    }
+  } else {
+    res.status(400).json({message: 'You have to provide a game id by a url param'});
+  }
+});
+// Start a game
+router.put('/game/start/:id', (req, res, next) => {
+  const id = req.params.id;
+  const setP = { $set: {isOpen: false, isInPlay: true}};
+  if (id) {
+    if (mongoose.Types.ObjectId.isValid(id)) {
+      Game.findByIdAndUpdate(id, setP, { new: true }).populate('questions')
+        .then(game => {
+          if (game) {
+            res.json(game);
+          } else {
+            res.status(404).json({ message: 'Game not found' });
+          }
+        })
+        .catch(err => next(err));
+    } else {
+      res.status(400).json({ message: 'Invalid game id' });
+    }
+  } else {
+    res.status(400).json({message: 'You have to provide a game id by a url param'});
+  }
+});
+// Finish a game
+router.put('/game/finish/:id', (req, res, next) => {
+  const id = req.params.id;
+  const setP = { $set: {isFinished: true, isInPlay: false}};
+  if (id) {
+    if (mongoose.Types.ObjectId.isValid(id)) {
+      Game.findByIdAndUpdate(id, setP, { new: true })
+        .then(game => {
+          if (game) {
+            res.json(game);
           } else {
             res.status(404).json({ message: 'Game not found' });
           }
