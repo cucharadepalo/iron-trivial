008a0779586465a87592cd64a1808eb1651b5496
Abraham Castro <cucharadepalo@gmail.com>
2017-12-11 11:30:02 +0100

    Initial commit

--- client/src/app/services/game.service.ts
+++ client/src/app/services/game.service.ts
@@ -1,8 +1,14 @@
+import { Game } from './../interfaces/game.interface';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class GameService {
+  private baseUrl = `${environment.apiUrl}/api`;
+  public game: Game;
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
 
 }
