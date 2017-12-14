8f3b6a9e3de307cd6a75f98034eef1c2a1b54d5e
Abraham Castro <cucharadepalo@gmail.com>
2017-12-14 13:36:33 +0100

    Individual results

--- client/src/app/components/results/results.component.ts
+++ client/src/app/components/results/results.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.scss']
 })
-export class ResultsComponent implements OnInit {
+export class ResultsComponent  {
 
-  constructor() { }
+  constructor(
+    public gameService: GameService
+  ) { }
 
-  ngOnInit() {
-  }
 
 }
